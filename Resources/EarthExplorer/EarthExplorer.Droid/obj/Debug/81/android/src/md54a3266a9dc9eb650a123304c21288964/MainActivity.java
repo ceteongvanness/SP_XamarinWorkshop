1b723b89662074bd06884b9a2f9c88e6e05e098d
Eng Teong Cheah <ceteongvanness@hotmail.com>
2019-01-12 13:57:32 +0800

    Update 02. Hands-on lab

---
+++ Resources/EarthExplorer/EarthExplorer.Droid/obj/Debug/81/android/src/md54a3266a9dc9eb650a123304c21288964/MainActivity.java
@@ -0,0 +1,47 @@
+package md54a3266a9dc9eb650a123304c21288964;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("EarthExplorer.Droid.MainActivity, EarthExplorer.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("EarthExplorer.Droid.MainActivity, EarthExplorer.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
